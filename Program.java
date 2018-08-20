0af67e39b571b5e3d1fa9e5d0ac4604e90066b07
lesamuka <42079758+lesamuka@users.noreply.github.com>
2018-08-19 22:40:56 -0300

    Tipos primitivos
    Alguns exemplos de tipos primitivos em java declarado no código.

---
+++ Program.java
@@ -0,0 +1,39 @@
+package course1;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean completed = false;
+		//true or false 1bit
+		char gender = 'F';
+		//Unicode character 16 bits
+		char letter = '\u0041';
+		//Signed integer 16 bits
+		byte n1 = 126;
+		//Signed integer 8 bits
+		int n2 = 1000;
+		//Signed integer 32 bits
+		int n3 = 2147483647;
+		//signet integer 32 bits
+		long n4 = 2147483648L;
+		//Signed integer 64 bits
+		float n5 = 4.5f;
+		//IEEE 754 floating point 32 bits
+		double n6 = 4.5;
+		//IEEE 754 floating point 64 bits
+		
+		System.out.println(completed);
+		System.out.println(gender);
+		System.out.println(letter);
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+		System.out.println(n5);
+		System.out.println(n6);
+
+	}
+
+}
